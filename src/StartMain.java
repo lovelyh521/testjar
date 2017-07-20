7d7e9b08a958be275126ad84083d74a7a1f51973
lyh <lyh_bxlt@163.com>
2017-07-20 22:49:15 +0800

    no message

---
+++ src/StartMain.java
@@ -0,0 +1,31 @@
+import excute.CreateFile;
+
+import java.io.*;
+
+public class StartMain {
+    public static void main(String[] args) {
+//        String path = "d:\\testjar\\out\\artifacts\\testjar_jar\\sourceFile";
+        String path = System.getProperty("user.dir")+ "\\sourceFile";
+        System.out.println("读取文件目录："+path);
+        File file = new File(path);
+        if(file != null){
+            File[] files = file.listFiles();
+            if(files != null){
+                for(File fl:files){
+                    if(fl.isFile()){
+                        try {
+                            CreateFile.creat2007Excel(fl.getPath(),fl.getName());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }else {
+                System.out.println("读取文件错误:请创建 sourceFile 文件夹并把需要读取的文件放入其中(注意大小写)。");
+            }
+
+        }/*else{
+            System.out.println("读取文件错误:请创建 sourceFile 文件夹并把需要读取的文件放入其中。");
+        }*/
+    }
+}
---
+++ src/excute/CreateFile.java
@@ -0,0 +1,158 @@
+package excute;
+
+import org.apache.poi.xssf.usermodel.*;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CreateFile {
+    public static String readFromFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        if (!file.exists() || file.isDirectory())
+            throw new FileNotFoundException();
+        FileInputStream fin = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fin, "GB2312");
+        BufferedReader br = new BufferedReader(isr);
+        String temp = null;
+        StringBuffer sb = new StringBuffer();
+        temp = br.readLine();
+        while (temp != null) {
+            sb.append(temp + "\r\n");
+            temp = br.readLine();
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * 创建2007版Excel文件
+     *
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public static void creat2007Excel(String filePath, String fileName) throws IOException {
+        XSSFWorkbook workBook = new XSSFWorkbook();
+        XSSFSheet sheet = workBook.createSheet();
+
+
+        File file = new File(filePath);
+        if (!file.exists() || file.isDirectory()){
+            throw new FileNotFoundException();
+        }
+
+        FileInputStream fin = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fin, "UTF-8");
+        BufferedReader br = new BufferedReader(isr);
+        String temp = null;
+        temp = br.readLine();
+        String rXc0 = "";
+
+        int i = 0;
+        boolean flag = false;
+        while (temp != null) {
+            if(temp.indexOf("No")!=-1){
+                if(flag){
+                    excute(fileName,workBook,sheet,i);
+                    i = 0;
+                }
+
+                flag = true;
+                temp = br.readLine();
+                continue;
+            }
+
+            if(temp.indexOf("MO SDR_OMMB") != -1){
+                rXc0 = temp.replace("-","");
+                temp = br.readLine();
+                continue;
+            }
+            if( temp.trim().equals("结果")|| temp.indexOf("管理对象标识")!=-1 || temp.indexOf("-----")!=-1){
+                if(temp.indexOf("管理对象标识")!=-1){
+                    String[] split = temp.split("\\s+");
+                    Column.column6 = split[1];
+                    Column.column7 = split[2];
+                    if(split.length>3){
+                        Column.column7 += " "+split[3];
+                    }
+                }
+                temp = br.readLine();
+                continue;
+            }
+            if(temp.indexOf("本次批处理") >=0){
+                excute(fileName,workBook,sheet,i);
+                break;
+            }
+            XSSFRow row = sheet.createRow(i+1);// 创建一个行对象
+            if(temp != null && !"".equals(temp.trim())){
+                String[] split = temp.trim().split("\\s+");
+                String[] split1 = split[0].split(",");
+                XSSFCell cell1 = row.createCell(0);// 创建单元格
+                cell1.setCellValue(rXc0);
+                Column.column1 = split1[0].split("=")[0];
+                Column.column2 = split1[1].split("=")[0];
+                Column.column3 = split1[2].split("=")[0];
+                Column.column4 = split1[3].split("=")[0];
+                Column.column5 = split1[4].split("=")[0];
+
+
+
+                for(int s1 = 0 ;s1<split1.length;s1++){
+                    XSSFCell cell2 = row.createCell(s1+1);// 创建单元格
+                    cell2.setCellValue(split1[s1].split("=")[1]);
+                }
+                for (int j=1 ; j<split.length;j++){
+                    XSSFCell cell = row.createCell(5+j);// 创建单元格
+                    cell.setCellValue(split[j]);
+                }
+
+                i++;
+            }
+
+            temp = br.readLine();
+        }
+
+    }
+    private static void excute( String fileName, XSSFWorkbook workBook, XSSFSheet sheet, int i) throws IOException {
+
+        XSSFRow rowTitle = sheet.createRow(0);// 创建一个行对象(表头)
+        rowTitle.createCell(1).setCellValue(Column.column1);
+        rowTitle.createCell(2).setCellValue(Column.column2);
+        rowTitle.createCell(3).setCellValue(Column.column3);
+        rowTitle.createCell(4).setCellValue(Column.column4);
+        rowTitle.createCell(5).setCellValue(Column.column5);
+        rowTitle.createCell(6).setCellValue(Column.column6);
+        rowTitle.createCell(7).setCellValue(Column.column7);
+
+        System.out.println("共转换 "+i+" 条数据");
+        // 文件输出流
+        SimpleDateFormat sf = new SimpleDateFormat("yyMMddhhmmss");
+        String format = sf.format(new Date());
+        String outFileName = fileName.substring(0, fileName.indexOf("."))+"-"+Column.column5+format;
+
+        File f = new File(".\\outputfile\\");
+        if(!f.exists()){
+            f.mkdir();
+        }
+        for(int k=0 ;k<8;k++){
+            sheet.autoSizeColumn(k);
+        }
+        FileOutputStream os = new FileOutputStream(".\\outputfile\\"+outFileName+".xlsx");
+//        FileOutputStream os = new FileOutputStream("d:\\"+outFileName+".xlsx");
+        workBook.write(os);// 将文档对象写入文件输出流
+
+        os.close();// 关闭文件输出流
+        System.out.println("创建成功:.\\outputfile\\"+outFileName+".xlsx");
+        workBook = new XSSFWorkbook();
+        sheet = workBook.createSheet();// 创建一个工作薄对象
+    }
+    private static class Column {
+        static String column1 = "";
+        static String column2 = "";
+        static String column3 = "";
+        static String column4 = "";
+        static String column5 = "";
+        static String column6 = "";
+        static String column7 = "";
+     }
+}
